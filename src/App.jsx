4530a8a0d82878f8bb69e6f23ab3b0b3e58ad7dd
Bidhu1024 <bidhu1024av@gmail.com>
2023-10-21 00:20:09 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      Hi all
+    </>
+  )
+}
+
+export default App
---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UserContext from "./userContext";
+
+
+
+const UserContextProvider = ({children}) => {
+  
+}
+
+export default UserContextProvider
